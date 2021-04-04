4c77532f8f50d65b3e0dfb8fe2d6608e3beb6c10
aamazan <armaan.amazan@gmail.com>
2021-04-03 20:40:07 -0700

    implemented basic file creation/text entry in console

--- fio.py
+++ fio.py
@@ -1,1 +1,18 @@
 # Description: program to relearn file i/o
+import csv
+
+name = input("Please enter a name for your .txt: \n")
+name += ".txt"
+f1 = open(name,"w")
+str = input("Please enter text for the test document. Type 0 and hit enter to quit. \n")
+
+while str != "0":
+    if str == "0":
+        break
+    else:
+        f1.write(str + "\n")
+        str = input()
+
+f1.close()
+
+#f2 = open("csvtest.txt", "w")
